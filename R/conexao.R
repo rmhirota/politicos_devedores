d3a1e8102c0d03c120edcd44f1bcb263c29d1075
Renata H <re.hirota@gmail.com>
2020-02-11 18:35:04 -0300

    arruma conexao metabase

--- R/conexao.R
+++
@@ -1,8 +0,0 @@
-# Conexão metabase
-library(RPostgreSQL)
-
-db <- dbConnect(PostgreSQL(),
-                user = "rows_user",
-                password = "oTSZ5ND4vTSxtdrkSyUz",
-                dbname = "postgres",
-                host = "vortex-metabase.ceojcho0vrjr.us-east-2.rds.amazonaws.com")
